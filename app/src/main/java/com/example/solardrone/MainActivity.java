9da65bf5ca5c39d63e1f39047616bc9173c652f1
juergen <juergenbenitez@gmail.com>
2021-03-27 15:02:55 -0400

    I added a location button to the main activity. I used nyc as an example to show on a google map.

--- app/src/main/java/com/example/solardrone/MainActivity.java
+++ app/src/main/java/com/example/solardrone/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.solardrone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,8 @@ import java.net.UnknownHostException;
 public class MainActivity extends AppCompatActivity {
     private TextView outputText;
     public String message;
+    TextView lat;
+    TextView lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected  Void doInBackground(Void...params){
             try {
-                s = new Socket("192.168.56.1",8888);
+                //changed below from 192.168.56.1 to 127.0.0.1
+                s = new Socket("192.168.1.30",8888);
                 pw =new PrintWriter(s.getOutputStream());
                 pw.write(message);
                 pw.flush();
@@ -65,5 +70,16 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+    public void showLocation(View v){
+        lat =(TextView) findViewById(R.id.LatitudeVal);
+        lon = (TextView) findViewById(R.id.LongitudeVal);
+
+        float latVal = Float.parseFloat(lat.getText().toString());
+        float lonVal = Float.parseFloat(lon.getText().toString());
+        Intent intent = new Intent(this,DroneLocationActivity.class);
+        intent.putExtra("latitude",latVal);
+        intent.putExtra("longitude",lonVal);
+        startActivity(intent);
     }
 }
