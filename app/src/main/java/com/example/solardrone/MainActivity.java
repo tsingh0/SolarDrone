68b0523b85f0111ca1d239e55503b57f6d6f8452
Taranvir <taranvirsingh97@gmail.com>
2021-04-01 18:29:48 -0400

    Added more buttons and functionalities

--- app/src/main/java/com/example/solardrone/MainActivity.java
+++ app/src/main/java/com/example/solardrone/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.solardrone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,7 +23,11 @@ import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView outputText;
+
     public String message;
+    private EditText inputText;
+    private Button sendButton1,battery;
+    private ImageView imageView;
     TextView lat;
     TextView lon;
 
@@ -28,25 +35,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText inputText = (EditText)findViewById(R.id.editText);
-        final Button clickable = (Button)findViewById(R.id.button);
-        clickable.setOnClickListener(new View.OnClickListener(){
+        inputText = (EditText)findViewById(R.id.editText);
+        sendButton1 = (Button)findViewById(R.id.sendButton);
+        battery = (Button)findViewById(R.id.battery);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageDrawable(null);
+       /* clickable.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                send sendcode = new send();
+                Send sendcode = new Send();
                 message=inputText.getText().toString();
                 sendcode.execute();
             }
-        });
+        });*/
     }
 
-    class send extends AsyncTask<Void,Void,Void>{
+    public void clear(View view){
+        imageView.setImageDrawable(null);
+    }
+
+    public void displayImage(View view){
+        Glide.with(this).load("https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/UAVs-help2.jpg").into(imageView);
+    }
+
+    public void askForBattery(View view) {
+        Send sendCode = new Send();
+        message="Battery";
+        sendCode.execute();
+    }
+
+
+    public void sendToServer(View view) {
+        Send sendCode = new Send();
+        message=inputText.getText().toString();
+        sendCode.execute();
+    }
+
+    public class Send extends AsyncTask<Void,Void,Void>{
         Socket s;
         PrintWriter pw;
         @Override
         protected  Void doInBackground(Void...params){
             try {
                 //changed below from 192.168.56.1 to 127.0.0.1
-                s = new Socket("192.168.1.30",8888);
+                s = new Socket("192.168.56.1",8888);
                 pw =new PrintWriter(s.getOutputStream());
                 pw.write(message);
                 pw.flush();
@@ -55,7 +86,8 @@ public class MainActivity extends AppCompatActivity {
                 BufferedReader br = new BufferedReader(in);
                 String str = br.readLine();
                 outputText = (TextView) findViewById(R.id.textOutput);
-                outputText.setText(str);
+                outputText.append(str+"\n");
+
 
                 System.out.println(str);
 
